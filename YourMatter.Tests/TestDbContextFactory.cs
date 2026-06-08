f8f57c547fa65210b098a77cf6f57fe6442774bd
Pengustus <orlin.rizov.bg@gmail.com>
2026-06-08 20:41:23 +0300

    Test dbcontext

---
+++ YourMatter.Tests/TestDbContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using YourMatter.Data.Data;
+
+namespace YourMatter.Tests
+{
+    public static class TestDbContextFactory
+    {
+        public static YourMatterDbContext Create(string dbName)
+        {
+            var options = new DbContextOptionsBuilder<YourMatterDbContext>()
+                .UseInMemoryDatabase(databaseName: dbName)
+                .Options;
+
+            var context = new YourMatterDbContext(options);
+            context.Database.EnsureCreated();
+            return context;
+        }
+    }
+}
